b6136b5978964929c260441d44dce34a914bb0b6
FrancisFire <34342660+FrancisFire@users.noreply.github.com>
2019-10-31 15:19:29 +0100

    [ADD] Preparazione all'utilizzo delle API di Maps

---
+++ ignite/lib/homepage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
