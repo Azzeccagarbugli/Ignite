3b4e2127fa7e7d8de01724ca15b0f932b86c4fd7
FrancisFire <34342660+FrancisFire@users.noreply.github.com>
2020-02-05 17:32:43 +0100

    [UPDATE] Inizializzata struttura per backend

---
+++ Backend/src/main/java/com/github/azzeccagarbugli/ignite/IgniteApplication.java
@@ -0,0 +1,13 @@
+package com.github.azzeccagarbugli.ignite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IgniteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IgniteApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/github/azzeccagarbugli/ignite/IgniteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.azzeccagarbugli.ignite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IgniteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
